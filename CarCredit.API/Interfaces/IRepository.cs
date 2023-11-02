a340ce22c8026d429b74e44cc1f5f77a99b799e2
IlyaOvchinnikov-0 <ilyaovchinnikov4@gmail.com>
2023-11-02 20:31:48 +0400

    Add base repository interface

---
+++ CarCredit.API/Interfaces/IRepository.cs
@@ -0,0 +1,13 @@
+﻿namespace CarCredit.API.Interfaces
+{
+    public interface IRepository<T>
+    {
+        bool Create(T entity); 
+
+        T Get(int id);
+
+        Task<IEnumerable<T>> GetAll();
+
+        bool Delete(T entity);
+    }
+}
